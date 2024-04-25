7c13d22c215926a231194576a73eadcfa8d1e33b
Sahil Ahmed <119150520+Zeddxx@users.noreply.github.com>
2024-04-25 21:14:44 +0530

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://enimi.onrender.com",
   })
 );
 
